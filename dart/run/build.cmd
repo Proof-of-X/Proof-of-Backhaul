59a85e9d3461152c2a7ace1f9765fd9541ac9869
Wanwiset Peerapatanapokin <wanwiset@gmail.com>
2023-04-18 10:06:06 +0700

    update scripts and workflow

--- dart/run/build.cmd
+++ dart/run/build.cmd
@@ -6,9 +6,11 @@ cd %dir%\..\src
 
 mkdir ..\bin > NUL
 
+cd pob
+
 echo const version = ''' > release.dart 
 git rev-parse HEAD >> release.dart
 echo '''; >> release.dart
 
-dart compile exe run-prover.dart	-o ../bin/run-prover.exe
-dart compile exe run-challenger.dart	-o ../bin/run-challenger.exe
+dart compile exe run-prover.dart	-o ../../bin/run-pob-prover.exe
+dart compile exe run-challenger.dart	-o ../../bin/run-pob-challenger.exe
